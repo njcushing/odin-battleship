e0961ab9ee425f74a49ded078320eaff5f57074b
njcushing <129661299+njcushing@users.noreply.github.com>
2023-07-10 21:21:48 +0100

    Write functionality and testing for placing ships on board

--- src/Gameboard/Gameboard.js
+++ src/Gameboard/Gameboard.js
@@ -11,14 +11,63 @@ const Gameboard = (s) => {
 
     const placeShip = (length, position, rotation) => {
         if (validateShipPosition(length, position, rotation)) {
-            return true;
+            const bounds = calculateShipArea(length, position, rotation);
+            if (rotation) {
+                for (let j = bounds[0][1]; j <= bounds[1][1]; j++) {
+                    board[j][position[0]] = 1;
+                }
+            } else {
+                for (let i = bounds[0][0]; i <= bounds[1][0]; i++) {
+                    board[position[1]][i] = 1;
+                }
+            }
         }
-        return false;
     };
 
     const moveShip = (originalPosition, destinationPosition, rotate) => {};
 
-    const validateShipPosition = (length, position, rotation) => {};
+    const validateShipPosition = (length, position, rotation) => {
+        if (!Number.isInteger(length) || length < 1 || length > size)
+            return false;
+        if (!Array.isArray(position) || position.length !== 2) return false;
+        if (!Number.isInteger(position[0])) return false;
+        if (!Number.isInteger(position[1])) return false;
+        if (typeof rotation !== "boolean") return false;
+
+        const bounds = calculateShipArea(length, position, rotation);
+
+        if (bounds[0][0] < 0 || bounds[0][0] >= size) return false;
+        if (bounds[0][1] < 0 || bounds[0][1] >= size) return false;
+        if (bounds[1][0] < 0 || bounds[1][0] >= size) return false;
+        if (bounds[1][1] < 0 || bounds[1][1] >= size) return false;
+
+        for (let i = bounds[0][0] - 1; i <= bounds[1][0] + 1; i++) {
+            if (i < -1 || i > size) return false;
+            if (i === -1 || i === size) continue;
+            for (let j = bounds[0][1] - 1; j <= bounds[1][1] + 1; j++) {
+                if (j < -1 || j > size) return false;
+                if (j === -1 || j === size) continue;
+                if (board[j][i] !== 0) return false;
+            }
+        }
+
+        return true;
+    };
+
+    const calculateShipArea = (length, position, rotation) => {
+        const lengthBehind = Math.abs(Math.floor((length - 1) / 2));
+        const lengthInfront = Math.abs(Math.ceil((length - 1) / 2));
+        if (rotation)
+            return [
+                [position[0], position[1] - lengthBehind],
+                [position[0], position[1] + lengthInfront],
+            ];
+        else
+            return [
+                [position[0] - lengthBehind, position[1]],
+                [position[0] + lengthInfront, position[1]],
+            ];
+    };
 
     const observeBoard = () => {
         return JSON.parse(JSON.stringify(board));
--- src/Gameboard/Gameboard.test.js
+++ src/Gameboard/Gameboard.test.js
@@ -1,6 +1,6 @@
 import Gameboard from "./Gameboard";
 
-test.only("Ensure board is being returned correctly", () => {
+test("Ensure board is being created & returned correctly", () => {
     const board1 = Gameboard(3);
     expect(board1.observeBoard()).toStrictEqual([
         [0, 0, 0],
@@ -18,5 +18,133 @@ test.only("Ensure board is being returned correctly", () => {
     expect(board3.observeBoard()).not.toStrictEqual([
         [0, 0],
         [0, 0],
+    ]);
+});
+
+test("Ensure board is not manipulated by invalid placeShip arguments", () => {
+    const board = Gameboard(8);
+    const boardSame = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    board.placeShip(-1, [3, 3], false);
+    board.placeShip(0, [3, 3], false);
+    board.placeShip(9, [3, 3], false);
+    board.placeShip(1.5, [3, 3], false);
+    board.placeShip("test", [3, 3], false);
+    expect(board.observeBoard()).toStrictEqual(boardSame);
+    board.placeShip(4, ["test", 3], false);
+    board.placeShip(4, [3, "test"], false);
+    board.placeShip(4, [-1, 3], false);
+    board.placeShip(4, [3, -1], false);
+    board.placeShip(4, [7, 3], false);
+    board.placeShip(4, [3, 9], false);
+    board.placeShip(4, [10, 3], false);
+    board.placeShip(4, [3, 10], false);
+    board.placeShip(4, [2, 0.4], false);
+    board.placeShip(4, [0.4, 2], false);
+    board.placeShip(4, "test", false);
+    expect(board.observeBoard()).toStrictEqual(boardSame);
+    board.placeShip(4, [3, 3], 2);
+    board.placeShip(4, [3, 3], 0.5);
+    board.placeShip(4, [3, 3], "test");
+    expect(board.observeBoard()).toStrictEqual(boardSame);
+});
+
+test("Ensure board is being manipulated correctly by valid placeShip arguments", () => {
+    const board = Gameboard(8);
+    board.placeShip(5, [3, 3], false);
+    expect(board.observeBoard()).toStrictEqual([
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+    board.placeShip(4, [1, 6], false);
+    expect(board.observeBoard()).toStrictEqual([
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+    board.placeShip(4, [7, 2], true);
+    expect(board.observeBoard()).toStrictEqual([
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 1, 1, 1, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+    board.placeShip(3, [5, 7], false);
+    expect(board.observeBoard()).toStrictEqual([
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 1, 1, 1, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+    board.placeShip(2, [7, 7], false);
+    expect(board.observeBoard()).toStrictEqual([
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 1, 1, 1, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+    board.placeShip(4, [0, 1], false);
+    expect(board.observeBoard()).toStrictEqual([
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 1, 1, 1, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+    board.placeShip(3, [0, 0], true);
+    expect(board.observeBoard()).toStrictEqual([
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 1, 1, 1, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+    board.placeShip(4, [2, 3], false);
+    expect(board.observeBoard()).toStrictEqual([
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 1, 1, 1, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
     ]);
 });
