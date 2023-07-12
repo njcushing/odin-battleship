bcdb0e682f295bfccebdd623cd5ff58c1004c993
njcushing <129661299+njcushing@users.noreply.github.com>
2023-07-12 22:24:28 +0100

    Write unit tests for the takeComputerTurnRandom method for Player

--- src/Player/Player.test.js
+++ src/Player/Player.test.js
@@ -1,4 +1,5 @@
 import Player from "./Player";
+import Gameboard from "./../Gameboard/Gameboard";
 
 test("Check setStyle method is changing the style of the Player correctly & only when argument is valid", () => {
     const player = Player();
@@ -11,4 +12,24 @@ test("Check setStyle method is changing the style of the Player correctly & only
     expect(player.getStyle()).toBe("Manual");
     player.setStyle("Computer");
     expect(player.getStyle()).toBe("Computer");
+});
+
+test("Check that the computer can make a random but not duplicate move using the takeComputerTurnRandom method", () => {
+    const player = Player();
+    const board = Gameboard(3);
+    player.setStyle("Computer");
+    player.takeComputerTurnRandom(board);
+    player.takeComputerTurnRandom(board);
+    player.takeComputerTurnRandom(board);
+    player.takeComputerTurnRandom(board);
+    player.takeComputerTurnRandom(board);
+    player.takeComputerTurnRandom(board);
+    player.takeComputerTurnRandom(board);
+    player.takeComputerTurnRandom(board);
+    player.takeComputerTurnRandom(board);
+    expect(board.observeBoard()).toStrictEqual([
+        [2, 2, 2],
+        [2, 2, 2],
+        [2, 2, 2],
+    ]);
 });
