321c8237956bba9dc3994efae2a32da47560a2df
njcushing <129661299+njcushing@users.noreply.github.com>
2023-07-13 09:15:59 +0100

    Write and pass unit tests for Game obj board creation

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,16 +1,24 @@
 import Player from "./../Player/Player";
+import Gameboard from "../Gameboard/Gameboard";
 
 const Game = () => {
     let playerOne = Player();
     let playerTwo = Player();
     playerTwo.setStyle("Computer");
+    let boardOne = Gameboard(10);
+    let boardTwo = Gameboard(10);
 
     const getPlayers = () => {
         return [playerOne, playerTwo];
     };
 
+    const getGameboards = () => {
+        return [boardOne, boardTwo];
+    };
+
     return {
         getPlayers,
+        getGameboards,
     };
 };
 export default Game;
--- src/Game/Game.test.js
+++ src/Game/Game.test.js
@@ -5,3 +5,21 @@ test("Ensure Game can create two Player objects from module and assign their def
     expect(game.getPlayers()[0].getStyle()).toBe("Manual");
     expect(game.getPlayers()[1].getStyle()).toBe("Computer");
 });
+
+test("Ensure Game can create two Gameboard objects from module", () => {
+    const game = Game();
+    const board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    expect(game.getGameboards()[0].observeBoard()).toStrictEqual(board);
+    expect(game.getGameboards()[1].observeBoard()).toStrictEqual(board);
+});
