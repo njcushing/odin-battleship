755a6203f7f92d2fc31bf97bab751834c22e9585
njcushing <129661299+njcushing@users.noreply.github.com>
2023-07-13 11:55:24 +0100

    Write unit tests for startGame method for Game module

--- src/Game/Game.test.js
+++ src/Game/Game.test.js
@@ -1,6 +1,11 @@
 import Game from "./../Game/Game";
+import computerAttack from "./__mocks__/computerAttack";
 
-test("Ensure Game can create two Player objects from module and assign their default play styles correctly", () => {
+jest.useFakeTimers();
+jest.spyOn(global, "setTimeout");
+
+test(`Ensure Game can create two Player objects from module and assign their
+    default play styles correctly`, () => {
     const game = Game();
     expect(game.getPlayers()[0].getStyle()).toBe("Manual");
     expect(game.getPlayers()[1].getStyle()).toBe("Computer");
@@ -22,4 +27,17 @@ test("Ensure Game can create two Gameboard objects from module", () => {
     ];
     expect(game.getGameboards()[0].observeBoard()).toStrictEqual(board);
     expect(game.getGameboards()[1].observeBoard()).toStrictEqual(board);
+});
+
+test(`When calling startGame method, make sure both boards are started, turn is
+    either 0 or 1 and computer takes turn if applicable`, () => {
+    const game = Game();
+    const boards = game.getGameboards();
+    boards[0].receiveAttack([3, 3]);
+    boards[1].receiveAttack([2, 2]);
+    expect(boards[0].observeBoard()).toStrictEqual(boards[1].observeBoard());
+    game.startGame();
+    const turn = game.getTurn();
+    expect([0, 1]).toContain(turn);
+    if (turn) expect(setTimeout).toHaveBeenCalledTimes(1);
 });
